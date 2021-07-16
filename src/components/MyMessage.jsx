509b62452978c1856b4537e9dc8259585fc0441d
Clint <maxeva02@gmail.com>
2021-07-15 20:27:26 -0700

    set up if and else statements with inline styling for users messages

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,15 @@
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+    if(message?.attachments?.length > 0) {
+        <img
+            src={message.attachments[0].file}
+            alt='message-attachment'
+            className='message-image'
+            style={{float:'right'}}
+        />
+    }
     return(
-        <div>
-            MyMessage
+        <div className='messgage' style={{float:'right', marginRight: '18px', color:'white', backgroundColor:'#3B2A50'}}>
+            {message.text}
         </div>
     );
 }
