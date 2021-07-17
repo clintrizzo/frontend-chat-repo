7c14fc24d48a87d5b7a0a285646a527b88b6c7d8
Clint <maxeva02@gmail.com>
2021-07-16 20:34:14 -0700

    set the values for the message form in th chatbox

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 const MessageForm = () => {
+    const [value, setValue] = useState('');
+    const handleSubmit = () => {
+
+    }
+    const handleChange = () => {
+        
+    }
     return(
-        <div>
-            Messageform
-        </div>
+        <form className='message-form' onSubmit={handleSubmit}>
+            <input 
+                className='message-input'
+                placeholder='Send a message ... '
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     );
 }
 
