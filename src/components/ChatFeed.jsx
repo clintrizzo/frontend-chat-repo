8fdb1ae6915bbbcb83aed984226854e29603b3e4
Clint <maxeva02@gmail.com>
2021-07-17 10:11:05 -0700

    updated message forms

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,55 +1,65 @@
-import MessageForm from './MessageForm';
 import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
+import MessageForm from './MessageForm';
 
 const ChatFeed = (props) => {
-    const { chats, activeChat, userName, messages } = props;
+  const { chats, activeChat, userName, messages } = props;
 
-    const chat = chats && chats[activeChat];
-    const renderMessages = () => {
-        const keys = Object.keys(messages);
-        return keys.map((key, index) => {
-            const message = messages[key]; {/*extracting from message block div */}
-            const lastMessageKey = index === 0 ? null : keys[index -1]; {/*extracting from message block div */}
-            const isMyMessage = userName === message.sender.username;
+  const chat = chats && chats[activeChat];
 
-            return (
-                <div key={`mesg_${index}`} style={{width: '100%'}}>
-                    <div className='message-block'>
-                        {/*passing the Mymessage as a prop from the message key
-                        TheirMessage is being passed using the same prop but is also using the set index for the users lastmessage*/}
-                        {
-                            isMyMessage 
-                            ? <MyMessage message={message}/> 
-                            :<TheirMessage message={message} lastMessage={messages[lastMessageKey]}/> 
-                        }
-                    </div>
-                    <div class='read-receipts' style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
-                        {/*If else statement for getting the properties aligned correctly */}
-                        read receipts
-                    </div>
-                </div>
-            )
-        })
-    }
+  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+    <div
+      key={`read_${index}`} /*extracting from message block div */
+      className="read-receipt" /*extracting from message block div */
+      style={{
+        float: isMyMessage ? 'right' : 'left',
+        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+      }}
+    />
+  ));
 
-    if(!chat) return 'Loading...'; {/*connected to line 40*/}
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
 
-    return(
-        <div className='chat-feed'>
-            <div className='chat-title-container'>
-                <div className='chat-title'>{chat?.title}</div> {/*question mark makes sure we can access the chat before the title variable*/}
-                <div className='chat-subtitle'>
-                    {chat.people.map((person) => ` ${person.person.username}`)}
-                </div>
-            </div>
-            {renderMessages()}
-            <div style={{height:'100px'}}/>
-            <div className='message-form-container'>
-                <MessageForm {...props} chatID={activeChat}/>
-            </div>
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+               {/*passing the Mymessage as a prop from the message key
+                TheirMessage is being passed using the same prop but is also using the set index for the users lastmessage*/}
+            {isMyMessage
+              ? <MyMessage message={message} />
+              : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
+          </div>
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+            {renderReadReceipts(message, isMyMessage)}
+          </div>
         </div>
-    )
-}
+      );
+    });
+  };
+
+  if (!chat) return <div />;
+
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  );
+};
 
 export default ChatFeed;
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,17 +1,20 @@
-const MyMessage = ({ message }) => {
-    if(message?.attachments?.length > 0) { /*seeing if the message is an image or text*/
+  const MyMessage = ({ message }) => {
+    if (message.attachments && message.attachments.length > 0) { /*seeing if the message is an image or text*/
+      return (
         <img
-            src={message.attachments[0].file} /*setting up image */
-            alt='message-attachment' 
-            className='message-image'
-            style={{float:'right'}}
+          src={message.attachments[0].file} /*setting up image */
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: 'right' }}
         />
+      );
     }
-    return(
-        <div className='messgage' style={{float:'right', marginRight: '18px', color:'white', backgroundColor:'#3B2A50'}}>
-            {message.text} {/*else statement for if message is not an image */}
-        </div>
+  
+    return (
+      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+        {message.text} {/*else statement for if message is not an image */}
+      </div>
     );
-}
-
-export default MyMessage;
+  };
+  
+  export default MyMessage;
